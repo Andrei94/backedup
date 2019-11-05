552caf32399e722d29ed65b1f63307473c8f0945
Andrei Precup <andreiprecup69@gmail.com>
2019-11-05 07:53:21 +0200

    Added Server-Side-Encryption to new objects added in the bucket

--- src/main/java/UploadObjectRequest.java
+++ src/main/java/UploadObjectRequest.java
@@ -1,11 +1,17 @@
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.StorageClass;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.logging.Logger;
 
 class UploadObjectRequest {
 	private String bucket;
 	private String remotePath;
 	private LocalFile localPathFile;
 	private String storageClass;
+	private Logger logger = Logger.getLogger(this.getClass().getName());
 
 	UploadObjectRequest withBucket(String bucketName) {
 		this.bucket = bucketName;
@@ -28,7 +34,20 @@ class UploadObjectRequest {
 	}
 
 	PutObjectRequest toS3PutObjectRequest() {
-		return new PutObjectRequest(bucket, remotePath, localPathFile.toFile()).withStorageClass(StorageClass.fromValue(storageClass));
+		try {
+			return new PutObjectRequest(bucket, remotePath, new FileInputStream(localPathFile.toFile()), objectMetadata())
+					.withStorageClass(StorageClass.fromValue(storageClass));
+		} catch(FileNotFoundException e) {
+			logger.warning(e.getMessage());
+			throw new IllegalArgumentException(e.getMessage());
+		}
+	}
+
+	private ObjectMetadata objectMetadata() {
+		ObjectMetadata objectMetadata = new ObjectMetadata();
+		objectMetadata.setContentLength(localPathFile.toFile().length());
+		objectMetadata.setSSEAlgorithm(ObjectMetadata.AES_256_SERVER_SIDE_ENCRYPTION);
+		return objectMetadata;
 	}
 
 	public String getBucket() {
