1d49d7ad097bf08d0bf3dba5416e5a93cef2dd80
Andrei Precup <andreiprecup69@gmail.com>
2019-11-14 08:12:36 +0200

    Increased size of main window

--- backedup-ui/src/main/java/Main.java
+++ backedup-ui/src/main/java/Main.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws IOException {
-		primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("mainWindow.fxml")), 400, 300));
+		primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("mainWindow.fxml")), 600, 400));
 		primaryStage.getScene().getStylesheets().add(getClass().getResource("button.css").toExternalForm());
 		primaryStage.show();
 	}
